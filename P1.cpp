4f11654e0f98701e3cea67cf93afbbf41706df97
Dio12334 <diego.paredes.r@utec.edu.pe>
2021-09-11 12:08:10 -0500

    P1 falta sus tests

--- P1.cpp
+++ P1.cpp
@@ -2,35 +2,75 @@
 #include <fstream>
 #include <cstring>
 #include <climits>
+#include <vector>
+
 using namespace std;
 
-class Alumno
+struct Alumno
 {
-public:
-	char Nombre [12];
-	char Apellidos [12];	
+	char codigo[5];
+	char nombre [11];
+	char apellidos [20];
+	char carrera[15];
 };
+
+istream & operator >> (istream & stream, Alumno & p)
+{	
+	stream.get(p.codigo, 5);		
+	stream.get(p.nombre, 11);
+	stream.get(p.apellidos, 20);
+	stream.get(p.carrera, 15);
+	return stream;
+}
 
 ostream & operator << (ostream & stream, Alumno & p)
 {	
-	stream << p.Nombre;
-	stream << p.Apellidos;
+	stream << p.codigo;
+	stream << p.nombre;
+	stream << p.apellidos;
+	stream << p.carrera;
 	stream << "\n";
 	stream << flush;
 	return stream;
 }
 
-istream & operator >> (istream & stream, Alumno & p)
-{	
-	stream.get(p.Nombre, 12);		
-	stream.get(p.Apellidos, 12);	
-	return stream;
-}
+class FixedRecord{
+	public:
+		FixedRecord(string fileName){
+			this->fileName = fileName;
+		}
+		vector<Alumno> load(){
+			inFile.open(fileName);
+			Alumno temp;
+			vector<Alumno> temps;
+			while(inFile >> temp){
+				temps.push_back(temp);
+			}
+			inFile.close();
+			return temps;
+		}
+		void add(Alumno record){
+			outFile.open(fileName);
+			outFile << record;
+			outFile.close();
+		}
+		Alumno readRecord(int pos){
+			inFile.open(fileName);
+			Alumno temp;
+			inFile.seekg(pos * sizeof(Alumno), ios::beg);
+			inFile >> temp; 
+			return temp;
+		}
+	private:
+		ofstream outFile;
+		ifstream inFile;
+		string fileName;
+};
 
 void readFromConsole(char buffer[], int size){
 	string temp;			
 	cin >> temp;
-	for(int i=0; i<size; i++)		
+	for(size_t i=0; i<(size_t)size; i++)		
 		buffer[i] = (i < temp.size())? temp[i] : ' ';
 	buffer[size-1] = '\0';
 	cin.clear();
@@ -38,25 +78,6 @@ void readFromConsole(char buffer[], int size){
 
 int main()
 {
-	ofstream outFile;
-	ifstream inFile;
-
-	//escribir
-	outFile.open("fixedlength.txt");
-	Alumno person1;	
-	//leer desde consola datos para person1
-	readFromConsole(person1.Nombre, 12);
-	readFromConsole(person1.Apellidos, 12);
-	outFile << person1;
-	outFile.close();
 	
-	//leer
-	inFile.open("fixedlength.txt");
-	Alumno person2;	
-	inFile >> person2;
-	cout << person2.Nombre;
-	cout << person2.Apellidos;
-	cin.get();
-	cout << endl;
 	return 0;
 }

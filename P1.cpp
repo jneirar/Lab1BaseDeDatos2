321994ae340aff12469f713b879ed2f7ea3de41c
Dio12334 <diego.paredes.r@utec.edu.pe>
2021-09-12 18:04:47 -0500

    p1 completado 100%

--- P1.cpp
+++ P1.cpp
@@ -13,27 +13,38 @@ struct Alumno
 	char apellidos [20];
 	char carrera[15];
 };
-
+//" sobrecarga {{{1
 istream & operator >> (istream & stream, Alumno & p)
 {	
-	stream.get(p.codigo, 5);		
-	stream.get(p.nombre, 11);
-	stream.get(p.apellidos, 20);
-	stream.get(p.carrera, 15);
+	stream.read(p.codigo, sizeof(p.codigo));
+	//stream.get();
+	stream.read(p.nombre, sizeof(p.nombre));
+	//stream.get();
+	stream.read(p.apellidos,sizeof(p.apellidos));
+	//stream.get();
+	stream.read(p.carrera, sizeof(p.carrera));
+	stream.get();
+	//stream.get();
+	
+	p.codigo[4] = '\0';
+  p.nombre[10] = '\0';
+  p.apellidos[19] = '\0';
+  p.carrera[14] = '\0';	
 	return stream;
 }
 
 ostream & operator << (ostream & stream, Alumno & p)
 {	
-	stream << p.codigo;
-	stream << p.nombre;
-	stream << p.apellidos;
-	stream << p.carrera;
+	stream.write(p.codigo, sizeof(p.codigo));
+	stream.write(p.nombre, sizeof(p.nombre));
+	stream.write(p.apellidos, sizeof(p.apellidos));
+	stream.write(p.carrera, sizeof(p.carrera));
 	stream << "\n";
 	stream << flush;
 	return stream;
 }
 
+//" FixedRecord {{{1
 class FixedRecord{
 	public:
 		FixedRecord(string fileName){
@@ -50,14 +61,14 @@ class FixedRecord{
 			return temps;
 		}
 		void add(Alumno record){
-			outFile.open(fileName);
+			outFile.open(fileName, ios::app);
 			outFile << record;
 			outFile.close();
 		}
 		Alumno readRecord(int pos){
 			inFile.open(fileName);
 			Alumno temp;
-			inFile.seekg(pos * sizeof(Alumno), ios::beg);
+			inFile.seekg(pos * (sizeof(temp)+1), ios::beg);
 			inFile >> temp; 
 			return temp;
 		}
@@ -69,15 +80,43 @@ class FixedRecord{
 
 void readFromConsole(char buffer[], int size){
 	string temp;			
-	cin >> temp;
+	getline(cin,temp);
 	for(size_t i=0; i<(size_t)size; i++)		
 		buffer[i] = (i < temp.size())? temp[i] : ' ';
 	buffer[size-1] = '\0';
 	cin.clear();
 }
 
+//" main {{{1
 int main()
 {
+	cout << "Antes del nuevo alumno" << endl;
+	FixedRecord f("datos12.txt");
+	vector<Alumno> vec = f.load();
+	for(size_t i = 0; i < vec.size(); i++)
+		cout<<vec[i].codigo <<" "<< vec[i].nombre <<" "<< vec[i].apellidos <<" "<< vec[i].carrera << endl;
 	
+	Alumno nuevo;
+	cout<< "Agregar un alumno" << endl;
+	cout<< "Ingresar codigo: ";
+	readFromConsole(nuevo.codigo, sizeof(nuevo.codigo));
+	cout<< "Ingresar nombre: ";
+	readFromConsole(nuevo.nombre, sizeof(nuevo.nombre));
+	cout<< "Ingresar apellidos: ";
+	readFromConsole(nuevo.apellidos, sizeof(nuevo.apellidos));
+	cout<< "Ingresar carrera: ";
+	readFromConsole(nuevo.carrera, sizeof(nuevo.carrera));
+	f.add(nuevo);
+
+	cout << "Despues de nuevo alumno" << endl;
+	vec = f.load();
+	for(size_t i = 0; i < vec.size(); i++)
+		cout<<vec[i].codigo <<" "<< vec[i].nombre <<" "<< vec[i].apellidos <<" "<< vec[i].carrera << endl;
+
+	int pos;
+	cout<< "Obtener registro en la posición: ";
+	cin>> pos;
+	Alumno r = f.readRecord(pos);
+	cout<<r.codigo <<" "<< r.nombre <<" "<< r.apellidos <<" "<< r.carrera << endl;
 	return 0;
 }
